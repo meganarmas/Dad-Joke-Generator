4de55b33f0272c38debf183a26105d4e7778e803
meganarmas <meganarmas@gmail.com>
2025-05-26 12:44:44 -0500

    Decided to make a navbar so I can make a few more generators for fun

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+function NavigationBar() {
+    return (
+        <Navbar>
+            <Container fluid>
+                
+            </Container>
+        </Navbar>
+    )
+}
